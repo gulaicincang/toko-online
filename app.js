c90851b83442625de29b4d241ab7367b01f8c9b0
gulaicincang <gulaicincang@gmail.com>
2018-11-18 14:30:46 +0700

    setting validator

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var config = require("./config/database");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+var expressValidator = require("express-validator");
 
 // Connection syntax
 mongoose.connect(config.database);
@@ -44,6 +45,44 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: true }
 }));
+
+// Setup express validator middleware
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    },
+    customValidators: {
+      isImage: function(value, filename) {
+        var extension = path.extname(filename).toLowerCase();
+        switch (extension) {
+          case ".jpg":
+            return ".jpg";
+          case ".jpeg":
+            return ".jpeg";
+          case ".png":
+            return ".png";
+          case "":
+            return ".jpg";
+          default:
+            return false;
+        }
+      }
+    }
+  })
+);
+
 
 
 //setup server
